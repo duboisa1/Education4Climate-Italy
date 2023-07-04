5a9b7f388594b9962b0bd030c318746a361948e7
duboisa1 <antoine-dubois@hotmail.com>
2023-07-04 14:23:15 +0200

    Updated scoring_output_explorer.py

--- settings.py
+++ settings.py
@@ -2,9 +2,10 @@ from os import path
 
 CRAWLING_OUTPUT_FOLDER = path.join('data', 'crawling-output', '')
 SCORING_OUTPUT_FOLDER = path.join('data', 'scoring-output', '')
+SCORING_ANALYSIS_FOLDER = path.join('data', 'scoring-analysis', '')
 WEB_INPUT_FOLDER = path.join('docs', 'data', '')
 PATTERNS_PATH = path.join('data', 'patterns', 'themes_patterns.json')
 
-YEAR = 2023
+YEAR = 2022
 
 ACCEPTED_LANGUAGES = ['en', 'it']
